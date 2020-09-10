02d6a979ea199116c16d246a2dbe0008fad924dd
Joao Victtor S Melo <joaovicttorsilveiramelo@gmail.com>
2020-09-10 20:00:50 -0300

    tirei necessidade da letra estar e minusculo

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -10,7 +10,7 @@ public class ChessPosition {
 	}
 
 	public ChessPosition(char column, int row) {
-		if (column< 'a' || column > 'h' ||column < 'A' || column > 'H' || row < 1 || row > 8) {
+		if (column< 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Erro instanting ChessPosition. Valid values are from a1 to h8");
 
 		}
